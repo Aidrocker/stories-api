d38a3febb77a12586ea7c3e3acb57b10d8aecda6
Aidar Maksimov <48646461+Aidrocker@users.noreply.github.com>
2021-02-03 11:02:21 +0300

    Add HistoryList with scroll

--- src/components/history/history-list/HistoryList.tsx
+++ src/components/history/history-list/HistoryList.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import 'src/styles/HistoryList.css';
 import HistoryItem from '../history-item/HistoryItem';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 
-const HistoryList:React.FC = () => {
+const HistoryList: React.FC = () => {
     return (
         <div className='history-list'>
-            <HistoryItem title='track.get'/>
+            
+            <Tabs
+                classes={{
+                    root: 'history-list'
+                }}
+                orientation="horizontal"
+                variant="scrollable"
+            >
+                <HistoryItem title='track.get' />
+                <HistoryItem title='track.get' />
+                <HistoryItem title='track.get' />
+                <HistoryItem title='track.get' />
+                <HistoryItem title='track.get' />
+                <HistoryItem title='track.get' />
+                <HistoryItem title='track.get' />
+                <HistoryItem title='track.get' />
+                <HistoryItem title='track.get' />
+                <HistoryItem title='track.get' />
+                <HistoryItem title='track.get' />
+                <HistoryItem title='track.get' />
+            </Tabs>
         </div>
     )
 }
